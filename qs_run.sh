ec868fd34016838560d61e81f6c464ce941ab1e4
Kyan Chen <chenkeyan@xiaohongshu.com>
2023-04-10 15:25:13 +0800

    add

---
+++ qs_run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+source ~/.bashrc
+conda activate torch1mmcv1  # torch1mmcv1 torch1mmcv2 torch2mmcv1 torch2mmcv2
+
+# pip install anypackage
+
+cd /mnt/search01/usr/chenkeyan/codes/lightning_framework
+python tools/train.py
+#python train.py
+#python -m torch.distributed.launch --nproc_per_node=$GPU_NUM --nnodes=$WORLD_SIZE --node_rank=$RANK --master_addr=$MASTER_ADDR --master_port=$MASTER_PORT --use_env train.py
+#python -m torch.distributed.launch --nproc_per_node=$GPU_NUM --nnodes=$WORLD_SIZE --node_rank=$RANK --master_addr=$MASTER_ADDR --master_port=$MASTER_PORT --use_env train_pipe.py
+# juicesync src dst
+# juicefs rmr your_dir
