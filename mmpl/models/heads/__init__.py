3dd2e82dc77c1afbd3021114348e1a881b5a537b
Kyan Chen <chenkeyan@xiaohongshu.com>
2023-04-12 18:00:37 +0800

    add

--- mmpl/models/heads/__init__.py
+++ mmpl/models/heads/__init__.py
@@ -1,4 +1,5 @@
 from .motiongpt_head import MotionGPTHead
 from .yolov8_sirens_head import YOLOv8SIRENSHead
+from .instance_match_head import InstanceMatchingHead
 
 __all__ = ['MotionGPTHead', 'YOLOv8SIRENSHead']
