fa5d355b2d20c2d838c7be525ae8b7190fb9eaf5
Kyan Chen <chenkeyan@xiaohongshu.com>
2023-04-14 10:45:17 +0800

    add

--- configs/seg/seg_just_backbone_config.py
+++ configs/seg/seg_just_backbone_config.py
@@ -178,9 +178,9 @@ test_pipeline = [
 ]
 
 
-train_batch_size_per_gpu = 4
+train_batch_size_per_gpu = 8
 train_num_workers = 2
-test_batch_size_per_gpu = 4
+test_batch_size_per_gpu = 8
 test_num_workers = 2
 persistent_workers = True
 
