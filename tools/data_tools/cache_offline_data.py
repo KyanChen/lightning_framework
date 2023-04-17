34a8873aac518507f7879605fe65e65ea7b1c729
Kyan Chen <chenkeyan@xiaohongshu.com>
2023-04-17 17:26:53 +0800

    add

--- tools/data_tools/cache_offline_data.py
+++ tools/data_tools/cache_offline_data.py
@@ -118,7 +118,7 @@ def main():
     cfg = Config.fromfile(args.config)
     if args.cfg_options is not None:
         cfg.merge_from_dict(args.cfg_options)
-    mmengine.mkdir_or_exist(args.out_dir)
+    mmengine.mkdir_or_exist(args.output_dir)
     # register all modules in mmcls into the registries
     register_all_modules()
     if args.phase == 'all':
