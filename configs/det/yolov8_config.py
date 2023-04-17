06c16d9407c3d8816b56833f9c81f691a3bbf535
Kyan Chen <chenkeyan@xiaohongshu.com>
2023-04-17 15:02:25 +0800

    add

--- configs/det/yolov8_config.py
+++ configs/det/yolov8_config.py
@@ -299,9 +299,9 @@ test_pipeline = [
                    'scale_factor', 'pad_param'))
 ]
 
-train_batch_size_per_gpu = 96
+train_batch_size_per_gpu = 80
 train_num_workers = 4
-test_batch_size_per_gpu = 96
+test_batch_size_per_gpu = 80
 test_num_workers = 4
 persistent_workers = True
 
