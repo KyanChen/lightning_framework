16a7a01ea18b011931a15930d48772f90b8fbfd9
Kyan Chen <chenkeyan@xiaohongshu.com>
2023-04-19 11:00:00 +0800

    add

--- mmpl/models/necks/__init__.py
+++ mmpl/models/necks/__init__.py
@@ -3,7 +3,7 @@ from .transformer_edecoder_neck import TransformerEDecoderNeck
 from .linear_proj import LinearProj
 from .hf_gpt_transformer_decoder import HFGPTTransformerDecoderNeck
 from .sirens import Sirens, ModulatedSirens
-from .sam_prompt_generator import SAMPromptGenNeck
+from .sam_prompt_generator import SAMPromptGenNeck, SAMPromptConvNeck
 
 __all__ = [
     'TransformerEncoderNeck', 'TransformerEDecoderNeck', 'LinearProj',
