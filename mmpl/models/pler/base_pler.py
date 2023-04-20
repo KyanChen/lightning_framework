0f4914d9e1f34373ce70ebb6b9ba8f70fc666cbf
Kyan Chen <chenkeyan@xiaohongshu.com>
2023-04-20 15:49:36 +0800

    add

--- mmpl/models/pler/base_pler.py
+++ mmpl/models/pler/base_pler.py
@@ -96,7 +96,7 @@ class BasePLer(pl.LightningModule, BaseModel):
 
             # set training parameters and lr
             for sub_model_name, value in sub_models.items():
-                sub_model_ = self.get_submodule(sub_model_name)
+                sub_model_ = self.trainer.model.get_submodule(sub_model_name)
                 if isinstance(sub_model_, torch.nn.Parameter):
                     # filter(lambda p: p.requires_grad, model.parameters())
                     sub_models[sub_model_name]['params'] = filter(lambda p: p.requires_grad, [sub_model_])
