ff0a4f8bf32568976689488f86eb7d388e842137
Kyan Chen <chenkeyan@xiaohongshu.com>
2023-04-20 10:52:13 +0800

    add

--- configs/seg/seg_sam_with_prompt_generator_sgd_config.py
+++ configs/seg/seg_sam_with_prompt_generator_sgd_config.py
@@ -179,7 +179,7 @@ trainer_cfg = dict(
     log_every_n_steps=10,
     check_val_every_n_epoch=1,
     benchmark=True,
-    sync_batchnorm=True,
+    # sync_batchnorm=True,
     # fast_dev_run=True,
 
     # limit_train_batches=1,
