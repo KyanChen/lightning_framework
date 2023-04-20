ec37d87bc09fea1ba7dfcfb50778218e25f6b983
Kyan Chen <chenkeyan@xiaohongshu.com>
2023-04-20 23:33:02 +0800

    add

--- configs/seg/seg_sam_with_prompt_generator_sgd_bs2_fp16_config.py
+++ configs/seg/seg_sam_with_prompt_generator_sgd_bs2_fp16_config.py
@@ -135,14 +135,14 @@ model_cfg = dict(
 )
 
 exp_name = 'E20230420_1'
-logger = dict(
-    type='WandbLogger',
-    project='building',
-    group='sam_prompt_generator',
-    name=exp_name
-)
+# logger = dict(
+#     type='WandbLogger',
+#     project='building',
+#     group='sam_prompt_generator',
+#     name=exp_name
+# )
 
-# logger = None
+logger = None
 
 callbacks = [
     param_scheduler_callback,
@@ -170,7 +170,7 @@ trainer_cfg = dict(
     # strategy='ddp_find_unused_parameters_true',
     # precision='32',
     precision='16-mixed',
-    devices=8,
+    devices=[6],
     default_root_dir=f'results/building/{exp_name}',
     # default_root_dir='results/tmp',
     max_epochs=max_epochs,
