18c78b2a136a77b849aeafb3681228a852b88743
Kyan Chen <chenkeyan@xiaohongshu.com>
2023-04-20 19:42:39 +0800

    add

--- tests/pytest.py
+++ tests/pytest.py
@@ -10,3 +10,16 @@ x = sharded_module(torch.randn(16, 3, 1024, 1024).cuda())
 loss = x.sum()
 loss.backward()
 optim.step()
+
+
+# import torch
+# torch.backends.cuda.matmul.allow_tf32 = False
+# torch.backends.cudnn.benchmark = True
+# torch.backends.cudnn.deterministic = False
+# torch.backends.cudnn.allow_tf32 = True
+# data = torch.randn([2, 3, 1024, 1024], dtype=torch.float, device='cuda', requires_grad=True)
+# net = torch.nn.Conv2d(3, 1280, kernel_size=[16, 16], padding=[0, 0], stride=[16, 16], dilation=[1, 1], groups=1)
+# net = net.cuda().float()
+# out = net(data)
+# out.backward(torch.randn_like(out))
+# torch.cuda.synchronize()
