1caa12a6329e6234ca1c65b1639176fd191cce43
Kyan Chen <chenkeyan@xiaohongshu.com>
2023-04-20 21:27:14 +0800

    add

--- tests/ddp_training/fsdp_pl.py
+++ tests/ddp_training/fsdp_pl.py
@@ -28,7 +28,7 @@ from module.segment_anything import sam_model_registry
 class MyModel(pl.LightningModule):
     def __init__(self):
         super().__init__()
-        self.res = torchvision.models.resnet50()
+        self.res = torchvision.models.resnet50().train()
         # self.sam = sam_model_registry['default']().eval().requires_grad_(False)
         # self.img_encoder = sam_model_registry['default']().image_encoder.eval().requires_grad_(False)
 
