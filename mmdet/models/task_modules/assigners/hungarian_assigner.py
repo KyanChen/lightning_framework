d9989da6954b934aa7ca8c7f21e648b1274314b1
Kyan Chen <chenkeyan@xiaohongshu.com>
2023-04-20 11:55:03 +0800

    add

--- mmdet/models/task_modules/assigners/hungarian_assigner.py
+++ mmdet/models/task_modules/assigners/hungarian_assigner.py
@@ -128,7 +128,11 @@ class HungarianAssigner(BaseAssigner):
         if linear_sum_assignment is None:
             raise ImportError('Please run "pip install scipy" '
                               'to install scipy first.')
-        matched_row_inds, matched_col_inds = linear_sum_assignment(cost)
+        try:
+            matched_row_inds, matched_col_inds = linear_sum_assignment(cost)
+        except:
+            import ipdb
+            ipdb.set_trace()
         matched_row_inds = torch.from_numpy(matched_row_inds).to(device)
         matched_col_inds = torch.from_numpy(matched_col_inds).to(device)
 
