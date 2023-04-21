1e4a9501a7c555113fe2a20fc9cf10aecd063bbb
Kyan Chen <chenkeyan@xiaohongshu.com>
2023-04-21 10:57:45 +0800

    add

--- mmpl/models/heads/__init__.py
+++ mmpl/models/heads/__init__.py
@@ -1,5 +1,5 @@
 from .motiongpt_head import MotionGPTHead
-# from .yolov8_sirens_head import YOLOv8SIRENSHead
+from .yolov8_sirens_head import YOLOv8SIRENSHead
 from .instance_match_head import InstanceMatchingHead
 from .semantic_seg_head import BinarySemanticSegHead
 
--- mmpl/models/pler/__init__.py
+++ mmpl/models/pler/__init__.py
@@ -1,8 +1,8 @@
 from .gpt_pler import GPTPLer
 from .motiongpt_pler import MotionGPTPLer
-# from .yolo_pler import YoloPLer
+from .yolo_pler import YoloPLer
 from .seg_pler import SegPLer
-# from .mmseg_pler import MMSegPLer
-# from .mmyolo_pler import MMYoloPLer
+from .mmseg_pler import MMSegPLer
+from .mmyolo_pler import MMYoloPLer
 
 # __all__ = ['GPTPLer', 'YoloPLer', 'SegPLer', 'MMSegPLer']
