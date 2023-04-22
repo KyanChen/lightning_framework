6c9cb157a6d41e4f3a9d854700ffd799fdd47c8e
Kyan Chen <chenkeyan@xiaohongshu.com>
2023-04-22 21:26:05 +0800

    add

--- configs/seg/seg_sambackbone_onelayer_head_bs2_config.py
+++ configs/seg/seg_sambackbone_onelayer_head_bs2_config.py
@@ -172,9 +172,9 @@ test_pipeline = [
 ]
 
 
-train_batch_size_per_gpu = 6
+train_batch_size_per_gpu = 8
 train_num_workers = 4
-test_batch_size_per_gpu = 6
+test_batch_size_per_gpu = 8
 test_num_workers = 4
 persistent_workers = True
 
