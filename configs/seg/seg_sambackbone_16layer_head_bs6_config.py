fe5ff6866fe76af2daaf4f2ee5d73fdf6620c2f5
Kyan Chen <chenkeyan@xiaohongshu.com>
2023-04-25 17:04:53 +0800

    add

--- configs/seg/seg_sambackbone_16layer_head_bs6_config.py
+++ configs/seg/seg_sambackbone_16layer_head_bs6_config.py
@@ -75,14 +75,14 @@ model_cfg = dict(
 )
 
 exp_name = 'E20230425_3'
-# logger = dict(
-#     type='WandbLogger',
-#     project='building',
-#     group='sam_backbone_only',
-#     name=exp_name
-# )
+logger = dict(
+    type='WandbLogger',
+    project='building',
+    group='sam_backbone_only',
+    name=exp_name
+)
 
-logger = None
+# logger = None
 
 callbacks = [
     param_scheduler_callback,
@@ -110,7 +110,7 @@ trainer_cfg = dict(
     # strategy='ddp_find_unused_parameters_true',
     # precision='32',
     # precision='16-mixed',
-    devices=1,
+    devices=8,
     default_root_dir=f'results/building/{exp_name}',
     # default_root_dir='results/tmp',
     max_epochs=max_epochs,
