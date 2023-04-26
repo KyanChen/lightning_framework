b4dd53c701cb6b81af02c4658ccc6259323213d0
Kyan Chen <chenkeyan@xiaohongshu.com>
2023-04-26 10:22:20 +0800

    add

--- configs/seg/seg_sambase_maskformerhead_12layer_bs6_config.py
+++ configs/seg/seg_sambase_maskformerhead_12layer_bs6_config.py
@@ -221,9 +221,9 @@ test_pipeline = [
 ]
 
 
-train_batch_size_per_gpu = 3
+train_batch_size_per_gpu = 8
 train_num_workers = 2
-test_batch_size_per_gpu = 3
+test_batch_size_per_gpu = 8
 test_num_workers = 2
 persistent_workers = True
 
