94bedbf29e01ea9be690b2074f2ed5cb2639a2f7
Kyan Chen <chenkeyan@xiaohongshu.com>
2023-04-28 10:33:23 +0800

    add

--- configs/seg/seg_maskformer_bs8_config.py
+++ configs/seg/seg_maskformer_bs8_config.py
@@ -168,14 +168,14 @@ model_cfg = dict(
 )
 
 exp_name = 'E20230428_0'
-# logger = dict(
-#     type='WandbLogger',
-#     project='building',
-#     group='maskformer',
-#     name=exp_name
-# )
+logger = dict(
+    type='WandbLogger',
+    project='building',
+    group='maskformer',
+    name=exp_name
+)
 
-logger = None
+# logger = None
 
 
 callbacks = [
@@ -204,7 +204,7 @@ trainer_cfg = dict(
     # strategy='ddp_find_unused_parameters_true',
     # precision='32',
     # precision='16-mixed',
-    devices=1,
+    devices=8,
     default_root_dir=f'results/building/{exp_name}',
     # default_root_dir='results/tmp',
     max_epochs=max_epochs,
