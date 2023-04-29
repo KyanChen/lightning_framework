5cf993455c574115ae150d1722f676094b9b2356
Kyan Chen <chenkeyan@xiaohongshu.com>
2023-04-29 22:56:52 +0800

    add

--- configs/ins_seg/seg_maskrcnn_nwpu_bs8_config.py
+++ configs/ins_seg/seg_maskrcnn_nwpu_bs8_config.py
@@ -130,7 +130,7 @@ model = dict(
             loss_bbox=dict(type='mmdet.L1Loss', loss_weight=1.0)),
         mask_roi_extractor=dict(
             type='mmdet.SingleRoIExtractor',
-            roi_layer=dict(type='mmdet.RoIAlign', output_size=14, sampling_ratio=0),
+            roi_layer=dict(type='RoIAlign', output_size=14, sampling_ratio=0),
             out_channels=256,
             featmap_strides=[4, 8, 16, 32]),
         mask_head=dict(
