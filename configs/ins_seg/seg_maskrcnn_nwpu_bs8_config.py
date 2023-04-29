9deff790bed7283aee104646748fb8de6d0d0db0
Kyan Chen <chenkeyan@xiaohongshu.com>
2023-04-29 23:23:05 +0800

    add

--- configs/ins_seg/seg_maskrcnn_nwpu_bs8_config.py
+++ configs/ins_seg/seg_maskrcnn_nwpu_bs8_config.py
@@ -302,9 +302,9 @@ test_pipeline = [
 ]
 
 
-train_batch_size_per_gpu = 8
+train_batch_size_per_gpu = 6
 train_num_workers = 2
-test_batch_size_per_gpu = 8
+test_batch_size_per_gpu = 6
 test_num_workers = 2
 persistent_workers = True
 
