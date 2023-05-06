56d23faaf8211607fde80f950f6eed80b4835595
Kyan Chen <chenkeyan@xiaohongshu.com>
2023-05-06 14:19:49 +0800

    add

--- tools/motion/get_lafan1_mean_std.py
+++ tools/motion/get_lafan1_mean_std.py
@@ -22,7 +22,7 @@ from mmpl.datasets.data_utils import lafan1_utils_torch
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Browse a dataset')
-    parser.add_argument('--config', default='configs/motion/motiongpt_uncertainty_meanstd_globalsystem_config.py', help='train config file path')
+    parser.add_argument('--config', default='configs/motion/motiongpt_uncertainty_meanstd_localsystem_config.py', help='train config file path')
     parser.add_argument(
         '--output-dir',
         '-o',
