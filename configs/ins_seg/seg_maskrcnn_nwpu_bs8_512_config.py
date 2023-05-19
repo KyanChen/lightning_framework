1d78a8350f9fe9e9545fac19fe8225c71854cd96
Kyan Chen <chenkeyan@xiaohongshu.com>
2023-05-19 21:45:28 +0800

    add

--- configs/ins_seg/seg_maskrcnn_nwpu_bs8_512_config.py
+++ configs/ins_seg/seg_maskrcnn_nwpu_bs8_512_config.py
@@ -1,7 +1,7 @@
 custom_imports = dict(imports=['mmseg.datasets', 'mmseg.models', 'mmdet.models'], allow_failed_imports=False)
 # train max 71, min 1
 # val max 56, min 1
-max_epochs = 1000
+max_epochs = 500
 
 optimizer = dict(
     type='AdamW',
@@ -57,7 +57,7 @@ evaluator = dict(
 )
 
 
-image_size = (1024, 1024)
+image_size = (512, 512)
 data_preprocessor = dict(
         type='mmdet.DetDataPreprocessor',
         mean=[123.675, 116.28, 103.53],
@@ -71,128 +71,131 @@ data_preprocessor = dict(
 num_things_classes = 10
 num_stuff_classes = 0
 num_classes = num_things_classes + num_stuff_classes
-num_queries = 80
 
 # model settings
 model = dict(
-    type='mmdet.Mask2Former',
+    type='mmdet.MaskRCNN',
     data_preprocessor=data_preprocessor,
     backbone=dict(
         type='mmdet.ResNet',
         depth=50,
         num_stages=4,
         out_indices=(0, 1, 2, 3),
-        frozen_stages=-1,
+        frozen_stages=1,
         norm_cfg=dict(type='BN', requires_grad=True),
         norm_eval=True,
         style='pytorch',
-        init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet50')),
-    panoptic_head=dict(
-        type='mmdet.Mask2FormerHead',
-        in_channels=[256, 512, 1024, 2048],  # pass to pixel_decoder inside
-        strides=[4, 8, 16, 32],
-        feat_channels=256,
+        init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet50')
+    ),
+    neck=dict(
+        type='mmdet.FPN',
+        in_channels=[256, 512, 1024, 2048],
         out_channels=256,
-        num_things_classes=num_things_classes,
-        num_stuff_classes=num_stuff_classes,
-        num_queries=num_queries,
-        num_transformer_feat_level=3,
-        pixel_decoder=dict(
-            type='mmdet.MSDeformAttnPixelDecoder',
-            num_outs=3,
-            norm_cfg=dict(type='GN', num_groups=32),
-            act_cfg=dict(type='ReLU'),
-            encoder=dict(  # DeformableDetrTransformerEncoder
-                num_layers=6,
-                layer_cfg=dict(  # DeformableDetrTransformerEncoderLayer
-                    self_attn_cfg=dict(  # MultiScaleDeformableAttention
-                        embed_dims=256,
-                        num_heads=8,
-                        num_levels=3,
-                        num_points=4,
-                        dropout=0.0,
-                        batch_first=True),
-                    ffn_cfg=dict(
-                        embed_dims=256,
-                        feedforward_channels=1024,
-                        num_fcs=2,
-                        ffn_drop=0.0,
-                        act_cfg=dict(type='ReLU', inplace=True)))),
-            positional_encoding=dict(num_feats=128, normalize=True)),
-        enforce_decoder_input_project=False,
-        positional_encoding=dict(num_feats=128, normalize=True),
-        transformer_decoder=dict(  # Mask2FormerTransformerDecoder
-            return_intermediate=True,
-            num_layers=9,
-            layer_cfg=dict(  # Mask2FormerTransformerDecoderLayer
-                self_attn_cfg=dict(  # MultiheadAttention
-                    embed_dims=256,
-                    num_heads=8,
-                    dropout=0.0,
-                    batch_first=True),
-                cross_attn_cfg=dict(  # MultiheadAttention
-                    embed_dims=256,
-                    num_heads=8,
-                    dropout=0.0,
-                    batch_first=True),
-                ffn_cfg=dict(
-                    embed_dims=256,
-                    feedforward_channels=2048,
-                    num_fcs=2,
-                    ffn_drop=0.0,
-                    act_cfg=dict(type='ReLU', inplace=True))),
-            init_cfg=None),
+        num_outs=5),
+    rpn_head=dict(
+        type='mmdet.RPNHead',
+        in_channels=256,
+        feat_channels=256,
+        anchor_generator=dict(
+            type='mmdet.AnchorGenerator',
+            scales=[8],
+            ratios=[0.5, 1.0, 2.0],
+            strides=[4, 8, 16, 32, 64]),
+        bbox_coder=dict(
+            type='mmdet.DeltaXYWHBBoxCoder',
+            target_means=[.0, .0, .0, .0],
+            target_stds=[1.0, 1.0, 1.0, 1.0]),
         loss_cls=dict(
-            type='mmdet.CrossEntropyLoss',
-            use_sigmoid=False,
-            loss_weight=2.0,
-            reduction='mean',
-            class_weight=[1.0] * num_classes + [0.1]),
-        loss_mask=dict(
-            type='mmdet.CrossEntropyLoss',
-            use_sigmoid=True,
-            reduction='mean',
-            loss_weight=5.0),
-        loss_dice=dict(
-            type='mmdet.DiceLoss',
-            use_sigmoid=True,
-            activate=True,
-            reduction='mean',
-            naive_dice=True,
-            eps=1.0,
-            loss_weight=5.0)),
-    panoptic_fusion_head=dict(
-        type='mmdet.MaskFormerFusionHead',
-        num_things_classes=num_things_classes,
-        num_stuff_classes=num_stuff_classes,
-        loss_panoptic=None,
-        init_cfg=None),
+            type='mmdet.CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
+        loss_bbox=dict(type='mmdet.L1Loss', loss_weight=1.0)),
+    roi_head=dict(
+        type='mmdet.StandardRoIHead',
+        bbox_roi_extractor=dict(
+            type='mmdet.SingleRoIExtractor',
+            roi_layer=dict(type='RoIAlign', output_size=7, sampling_ratio=0),
+            out_channels=256,
+            featmap_strides=[4, 8, 16, 32]),
+        bbox_head=dict(
+            type='mmdet.Shared2FCBBoxHead',
+            in_channels=256,
+            fc_out_channels=1024,
+            roi_feat_size=7,
+            num_classes=num_classes,
+            bbox_coder=dict(
+                type='mmdet.DeltaXYWHBBoxCoder',
+                target_means=[0., 0., 0., 0.],
+                target_stds=[0.1, 0.1, 0.2, 0.2]),
+            reg_class_agnostic=False,
+            loss_cls=dict(
+                type='mmdet.CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
+            loss_bbox=dict(type='mmdet.L1Loss', loss_weight=1.0)),
+        mask_roi_extractor=dict(
+            type='mmdet.SingleRoIExtractor',
+            roi_layer=dict(type='RoIAlign', output_size=14, sampling_ratio=0),
+            out_channels=256,
+            featmap_strides=[4, 8, 16, 32]),
+        mask_head=dict(
+            type='mmdet.FCNMaskHead',
+            num_convs=4,
+            in_channels=256,
+            conv_out_channels=256,
+            num_classes=num_classes,
+            loss_mask=dict(
+                type='mmdet.CrossEntropyLoss', use_mask=True, loss_weight=1.0))),
+    # model training and testing settings
     train_cfg=dict(
-        num_points=12544,
-        oversample_ratio=3.0,
-        importance_sample_ratio=0.75,
-        assigner=dict(
-            type='mmdet.HungarianAssigner',
-            match_costs=[
-                dict(type='mmdet.ClassificationCost', weight=2.0),
-                dict(
-                    type='mmdet.CrossEntropyLossCost', weight=5.0, use_sigmoid=True),
-                dict(type='mmdet.DiceCost', weight=5.0, pred_act=True, eps=1.0)
-            ]),
-        sampler=dict(type='mmdet.MaskPseudoSampler')),
+        rpn=dict(
+            assigner=dict(
+                type='mmdet.MaxIoUAssigner',
+                pos_iou_thr=0.7,
+                neg_iou_thr=0.3,
+                min_pos_iou=0.3,
+                match_low_quality=True,
+                ignore_iof_thr=-1),
+            sampler=dict(
+                type='mmdet.RandomSampler',
+                num=256,
+                pos_fraction=0.5,
+                neg_pos_ub=-1,
+                add_gt_as_proposals=False),
+            allowed_border=-1,
+            pos_weight=-1,
+            debug=False),
+        rpn_proposal=dict(
+            nms_pre=2000,
+            max_per_img=1000,
+            nms=dict(type='nms', iou_threshold=0.7),
+            min_bbox_size=0),
+        rcnn=dict(
+            assigner=dict(
+                type='mmdet.MaxIoUAssigner',
+                pos_iou_thr=0.5,
+                neg_iou_thr=0.5,
+                min_pos_iou=0.5,
+                match_low_quality=True,
+                ignore_iof_thr=-1),
+            sampler=dict(
+                type='mmdet.RandomSampler',
+                num=512,
+                pos_fraction=0.25,
+                neg_pos_ub=-1,
+                add_gt_as_proposals=True),
+            mask_size=28,
+            pos_weight=-1,
+            debug=False)),
     test_cfg=dict(
-        panoptic_on=False,
-        # For now, the dataset does not support
-        # evaluating semantic segmentation metric.
-        semantic_on=False,
-        instance_on=True,
-        # max_per_image is for instance segmentation.
-        max_per_image=100,
-        iou_thr=0.8,
-        # In Mask2Former's panoptic postprocessing,
-        # it will filter mask area where score is less than 0.5 .
-        filter_low_score=True),
-    init_cfg=None)
+        rpn=dict(
+            nms_pre=1000,
+            max_per_img=1000,
+            nms=dict(type='nms', iou_threshold=0.7),
+            min_bbox_size=0),
+        rcnn=dict(
+            score_thr=0.05,
+            nms=dict(type='nms', iou_threshold=0.5),
+            max_per_img=100,
+            mask_thr_binary=0.5)
+    )
+)
 
 
 model_cfg = dict(
@@ -206,11 +209,11 @@ model_cfg = dict(
 )
 
 task_name = 'nwpu_ins'
-exp_name = 'E20230519_4'
+exp_name = 'E20230520_1'
 logger = dict(
     type='WandbLogger',
     project=task_name,
-    group='mask2former',
+    group='maskrcnn',
     name=exp_name
 )
 # logger = None
@@ -304,9 +307,9 @@ test_pipeline = [
 ]
 
 
-train_batch_size_per_gpu = 4
+train_batch_size_per_gpu = 8
 train_num_workers = 4
-test_batch_size_per_gpu = 4
+test_batch_size_per_gpu = 8
 test_num_workers = 4
 persistent_workers = True
 
