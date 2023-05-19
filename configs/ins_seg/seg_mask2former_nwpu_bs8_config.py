92563f883cdbbc0cb3b8b4ba217c95bfe1485119
Kyan Chen <chenkeyan@xiaohongshu.com>
2023-05-19 15:19:40 +0800

    add

--- configs/ins_seg/seg_mask2former_nwpu_bs8_config.py
+++ configs/ins_seg/seg_mask2former_nwpu_bs8_config.py
@@ -83,7 +83,7 @@ model = dict(
         num_stages=4,
         out_indices=(0, 1, 2, 3),
         frozen_stages=-1,
-        norm_cfg=dict(type='BN', requires_grad=False),
+        norm_cfg=dict(type='BN', requires_grad=True),
         norm_eval=True,
         style='pytorch',
         init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet50')),
@@ -206,7 +206,7 @@ model_cfg = dict(
 )
 
 task_name = 'nwpu_ins'
-exp_name = 'E20230519_3'
+exp_name = 'E20230519_4'
 logger = dict(
     type='WandbLogger',
     project=task_name,
