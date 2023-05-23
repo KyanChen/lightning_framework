74dfc552c2fcc3ffafe8e64168b901a850910f5f
Kyan Chen <chenkeyan@xiaohongshu.com>
2023-05-23 17:56:27 +0800

    add

--- configs/ins_seg/seg_sam_prompt_nwpu_bs2_decoder_config.py
+++ configs/ins_seg/seg_sam_prompt_nwpu_bs2_decoder_config.py
@@ -270,9 +270,9 @@ test_pipeline = [
 ]
 
 
-train_batch_size_per_gpu = 3
+train_batch_size_per_gpu = 2
 train_num_workers = 2
-test_batch_size_per_gpu = 3
+test_batch_size_per_gpu = 2
 test_num_workers = 2
 persistent_workers = True
 
